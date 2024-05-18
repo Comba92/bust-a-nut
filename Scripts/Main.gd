415e0b1433b79a1bf887e7e92843d8a36376590b
Comba <comba@cock.li>
2024-05-18 13:19:27 +0200

    Fixed (probably) projectile alignin

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -4,8 +4,8 @@ var Bubble = preload("res://Scenes/Bubble_Fixed.tscn")
 var Projectile = preload("res://Scenes/Bubble_Projectile.tscn")
 @onready var bubbles = $Bubbles
 
-@export var max_width = 5
-@export var max_height = 3
+@export var max_width = 10
+@export var max_height = 2
 
 # se siamo sulla riga pari, in alto e in basso controllo a sinistra e in centro
 # se siamo sulla riga dispari, in alto e in basso controllo in centro e a destra
@@ -35,19 +35,19 @@ func _ready() -> void:
       bubble.set_color(Globals.colors.pick_random())
       print(bubble.position)
       
-  for b in bubbles.get_children():
-    find_neighbors(b)
+    find_neighbors()
       
-func find_neighbors(bubble) -> void:
-  var is_even_row = roundi(bubble.grid_position.y) % 2 == 0
-  var directions = even_row_directions if is_even_row else odd_row_directions
-  
-  bubble.neighbors = []
-  for d in directions:
-    var p = bubble.grid_position + d
-    var s = 'Bubble' + str(p)
-    var b = bubbles.get_node(s)
-    if b: bubble.neighbors.append(b)
+func find_neighbors() -> void:
+  for bubble in bubbles.get_children():
+    var is_even_row = roundi(bubble.grid_position.y) % 2 == 0
+    var directions = even_row_directions if is_even_row else odd_row_directions
+    
+    bubble.neighbors = []
+    for d in directions:
+      var p = bubble.grid_position + d
+      var s = 'Bubble' + str(p)
+      var b = bubbles.get_node(s)
+      if b: bubble.neighbors.append(b)
 
 func dfs(bubble, visited) -> Dictionary:
   visited[bubble] = true
@@ -59,18 +59,17 @@ func dfs(bubble, visited) -> Dictionary:
 
 func explode_bubbles(bubble) -> void:
   var visited = dfs(bubble, {})
-  print(visited)
   if visited.size() < 3: return
   for b in visited:
     bubbles.remove_child(b)
+    
+  find_neighbors()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
   if Input.is_action_just_pressed("ui_down"):
     for b in bubbles.get_children():
       explode_bubbles(b)
-    for b in bubbles.get_children():
-      find_neighbors(b)
 
 func _on_cannon_shoot(projectile) -> void:
   add_child(projectile)
@@ -81,20 +80,15 @@ func _on_projectile_bubble_touched(proj) -> void:
   
   var bubble = Bubble.instantiate()
   bubbles.add_child(bubble)
-  # var row_height = roundi(bubble.diameter * Globals.triangular_height)
-  # bubble.position.y = roundi((proj.global_position.y - bubbles.global_position.y) / row_height) * row_height
-  # var offset = 0 if floori(bubble.position.y / row_height) % 2 == 0 else bubble.radius
-  # bubble.position.x = roundi((proj.global_position.x - bubbles.global_position.x - offset) / bubble.diameter) * bubble.diameter + offset
+
+  # PERFECTION
+  var grid_pos = bubble.world_pos_to_grid(proj.global_position - bubbles.global_position)
+  bubble.position.y = grid_pos.y * (bubble.diameter * Globals.triangular_height)
+  var offset = 0 if roundi(grid_pos.y) % 2 == 0 else bubble.radius
+  bubble.position.x = grid_pos.x * (bubble.diameter) + offset
   
-  bubble.position = bubble.align_pos_to_grid(proj.global_position, bubbles.global_position)
   bubble.set_grid_position(bubble.position)
   bubble.set_color(proj.color)
-  
-  for b in bubbles.get_children():
-    find_neighbors(b)
-    
-  explode_bubbles(bubble)
-  
-  for b in bubbles.get_children():
-    find_neighbors(b)
+  find_neighbors()
+  # explode_bubbles(bubble)
 
